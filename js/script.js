44f3ee38cb67ea691f02b38ef47aa21c239971ca
bhavinbandhiya <bhavinbandhiya003@gmail.com>
2021-09-14 18:54:12 +0530

    adddaytextcontent

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+let searchInp = document.querySelector('.weather_search');
+let city = document.querySelector('.weather_city');
+let day = document.querySelector('.weather_day');
+let hummidity = document.querySelector('.weather_indicator--hummidity>.value');
+let wind = document.querySelector('.weather_indicator--wind>.value');
+let pressure = document.querySelector('.weather_indicator--pressure>.value');
+let image = document.querySelector('.weather_image');
+let temperature = document.querySelector('.weather_temperature');
 let weatherAPIKey = '749764725e75169d71f289ceab73d6ee';
 let weatherBaseEndpoint = 'https://api.openweathermap.org/data/2.5/weather?units=metric&appid=' + weatherAPIKey;
 
@@ -6,7 +14,20 @@ let getWeatherByCityName = async (city) => {
     let response = await fetch(endpoint);
     let weather = await response.json();
 
-    console.log(weather);
+    return weather;
 }
 
-getWeatherByCityName('New York');
+searchInp.addEventListener('keydown', async (e) => {
+    if(e.keyCode === 13) {
+      let weather =await getWeatherByCityName(searchInp.value);
+      updateCurrentWeather(weather);
+    }
+});
+
+let updateCurrentWeather = (data) => {
+    city.textContent = data.name + ', ' + data.sys.country;
+    day.textContent = dayOfweek();
+}
+let dayOfweek = (data) => {
+   return new Date().toLocaleDateString('en-EN', {'weekday':'long'});
+}
